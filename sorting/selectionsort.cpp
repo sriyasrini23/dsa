c0789a7c753d4bcf1253a7c9a23550c6d59d04f2
sriyasrini23 <sriyasrini238@gmail.com>
2024-06-03 15:51:40 +0530

    added selection sort

---
+++ sorting/selectionsort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include "print.h"
+using namespace std;
+
+void selectionSort(vector<int>& arr, int n){
+        for(int i=0; i<n; i++){
+            int key = i;
+            int keyvalue = arr[i];
+            for(int j=i; j<n; j++){
+                if(arr[j]<arr[key]){
+                    key = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[key];
+            arr[key] = temp;
+        }
+}
+
+int main()
+{   
+    vector<int> arr({8, 9, 2, 3, 5, 7, 4});
+    int n = arr.size();
+    selectionSort(arr, n);
+    printArray<int>(arr, n);
+    return 0;
+}
