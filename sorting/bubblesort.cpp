5f66c72af739a08dccb9c9ca3bdffa9c92855fa1
sriyasrini23 <sriyasrini238@gmail.com>
2024-06-04 11:27:34 +0530

    added bubblesort

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 void bubbleSort(vector<int>& arr, int n){
+    bool swapped = false;
     for(int i=0; i<n; i++){
-        for(int j=0; j<(n-1); j++){
+        for(int j=0; j<(n-i-1); j++){ //no need to check till n-1 each time
             if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
